68d704bd09205a5f950f65554262a2f3f0cc0bc6
Jakub Zohar Rutkowski <kuba@skb.pl>
2023-02-22 09:46:05 +0100

    Added platform settings interface

---
+++ ITBees.Interfaces/Platforms/IPlatformSettingsService.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace ITBees.Interfaces.Platforms
+{
+    public interface IPlatformSettingsService
+    {
+        List<string> GetPlatformDebugEmails();
+        Environment GetCurrentEnvironment();
+    }
+
+    public enum Environment
+    {
+        Dev,
+        Uat,
+        Prod
+    }
+}
