a66047d1154c7019988b54a60793397a4608b09d
Jakub Zohar Rutkowski <kuba@skb.pl>
2023-02-22 06:24:05 +0100

    Add project files.

---
+++ ITBees.Interfaces/Repository/IReadOnlyRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace ITBees.Interfaces.Repository
+{
+    public interface IReadOnlyRepository<T>
+    {
+
+        bool HasData(Expression<Func<T, bool>> predicate);
+        T GetFirst(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);
+
+        ICollection<T> GetData(Expression<Func<T, bool>> predicate,
+            params Expression<Func<T, object>>[] includeProperties);
+
+        int GetDataCount(Expression<Func<T, bool>> predicate);
+
+        IQueryable<T> GetDataQueryable(Expression<Func<T, bool>> predicate);
+        IQueryable<T> GetDataQueryable(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);
+        ICollection<T> GetDataFromStoredProcedure(string procedureName, params object[] procedureArgument);
+        ICollection<T2> Sql<T2>(string sql) where T2 : class;
+    }
+}
---
+++ ITBees.Interfaces/Repository/IWriteOnlyRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+
+namespace ITBees.Interfaces.Repository
+{
+    public interface IWriteOnlyRepository<T>
+    {
+        T InsertData(T entity);
+        ICollection<T> InsertData(ICollection<T> includeProperty);
+
+        ICollection<T> UpdateData(Expression<Func<T, bool>> predicate, Action<T> updateAction,
+            params Expression<Func<T, object>>[] includeProperties);
+
+        int DeleteData(Expression<Func<T, bool>> predicate);
+        void DeleteData(Expression<Func<T, bool>> predicate, params Expression<Func<T, object>>[] includeProperties);
+        void Sql(string sql);
+    }
+
+    public interface IWriteOnlyRepository<T, TContext> : IWriteOnlyRepository<T> where TContext : DbContext
+    {
+        void Dispose();
+    }
+}
