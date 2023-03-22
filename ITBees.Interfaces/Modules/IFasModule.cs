820556f8fc806420c0027a7517c29b30d2552e79
Jakub Zohar Rutkowski <kuba@skb.pl>
2023-03-22 08:11:12 +0100

    Moved IFasModule interface

---
+++ ITBees.Interfaces/Modules/IFasModule.cs
@@ -0,0 +1,10 @@
+﻿namespace ITBees.Interfaces.Modules
+{
+    /// <summary>
+    /// Marker interface means the services for which we want to define access levels for individual users. For example, the UserManager.NewUserRegistrationService service should be marked as IFasModule so that it is possible to define who can create new users within the organization in other service.
+    /// </summary>
+    public interface IFasModule
+    {
+        
+    }
+}
