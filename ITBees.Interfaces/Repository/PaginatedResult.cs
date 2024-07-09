6bc5d9f07513747f70c9975ab5dd4c197d0cb58f
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-07-09 07:27:16 +0200

    change data type for result

--- ITBees.Interfaces/Repository/PaginatedResult.cs
+++ ITBees.Interfaces/Repository/PaginatedResult.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections.Generic;
 
+namespace ITBees.Interfaces.Repository;
+
 public class PaginatedResult<T>
 {
     public int AllElementsCount { get; set; }
     public int CurrentPage { get; set; }
     public int ElementsPerPage { get; set; }
-    public List<T> Data { get; set; }
+    public ICollection<T> Data { get; set; }
 }
