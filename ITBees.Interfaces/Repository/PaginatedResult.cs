423c41d30b44f5a65719f91948fda6fae0686ddd
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-07-09 07:53:34 +0200

    Add all pages count

--- ITBees.Interfaces/Repository/PaginatedResult.cs
+++ ITBees.Interfaces/Repository/PaginatedResult.cs
@@ -5,6 +5,7 @@ namespace ITBees.Interfaces.Repository;
 public class PaginatedResult<T>
 {
     public int AllElementsCount { get; set; }
+    public int AllPagesCount { get; set; }
     public int CurrentPage { get; set; }
     public int ElementsPerPage { get; set; }
     public ICollection<T> Data { get; set; }
