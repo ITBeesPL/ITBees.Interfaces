6fdb99f099020ab9fb051310f0cd4bb19b8da09c
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-09-24 21:04:01 +0200

    Support for language factory

---
+++ ITBees.Interfaces/Lang/ILanguageFactory.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using ITBees.Models.Languages;
+
+namespace ITBees.Interfaces.Lang;
+
+public interface ILanguageFactory
+{
+    void LoadData(List<Language> languages);
+    Language Get(string language);
+}
