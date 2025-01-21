f38a8a68fc28e138090a6a43a4e960768be683d9
Jakub Zohar Rutkowski <kuba@skb.pl>
2025-01-21 15:59:49 +0100

    Nullable string property field

---
+++ ITBees.Interfaces/CodeGeneration/NullableStringPropertyAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace ITBees.Interfaces.CodeGeneration;
+
+/// <summary>
+/// Use this decorator if You want to have generated 'string?' types when using typescript generator  
+/// </summary>
+[AttributeUsage(AttributeTargets.Property)]
+public class NullableStringPropertyAttribute : Attribute
+{
+}
