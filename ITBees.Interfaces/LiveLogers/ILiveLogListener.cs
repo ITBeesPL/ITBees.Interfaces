d39ed2071144c6b251f9fb6857adcea079c8a4ef
Jakub Zohar Rutkowski <kuba@skb.pl>
2025-11-27 10:23:33 +0100

    Add TargetCashHolder property to CashReceivedIm and improve logging in Ingenico services

---
+++ ITBees.Interfaces/LiveLogers/ILiveLogListener.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace ITBees.Interfaces.Logs;
+
+public interface ILiveLogListener
+{
+    Task LogMessage(string message);
+    Task LogErrorMessage(string message);
+}
