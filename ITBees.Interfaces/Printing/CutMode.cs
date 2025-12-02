9940131a4670efe32b72eb6ecc2fb778f420dea2
Jakub Zohar Rutkowski <kuba@skb.pl>
2025-12-02 17:08:25 +0100

    Add CutMode enum and IPcsEscPosPrinter interface for printing functionality

---
+++ ITBees.Interfaces/Printing/CutMode.cs
@@ -0,0 +1,7 @@
+﻿namespace ITBees.Interfaces.Printing;
+
+public enum CutMode
+{
+    Full = 0x00, // GS V 0 – full cut
+    Partial = 0x01 // GS V 1 – partial cut
+}
---
+++ ITBees.Interfaces/Printing/IPcsEscPosPrinter.cs
@@ -0,0 +1,38 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace ITBees.Interfaces.Printing;
+
+public interface IPcsEscPosPrinter
+{
+    void AlignRight();
+    void AlignLeft();
+    void CutOff(CutMode mode = CutMode.Full);
+    void SetLargeBoldFont();
+    void SetLargeFont();
+    void AddLineToPrintStream(string text);
+    void SetMediumBoldFont();
+    Task Print(string port);
+    void SetCodePage(MemoryStream ms, byte codePageNumber);
+    void WriteTextLine(string text, MemoryStream ms);
+    void AddEmptyLines(MemoryStream ms, int linesCount);
+    void FeedLines(int linesCount);
+    void AlignCenter();
+    void SetStandardFontWeight();
+    void SetStandardFont();
+    void SetBoldFont();
+    void SetMediumFont();
+    void WriteByte(byte[] bytes);
+    void Write(byte[] data, int i, int dataLength);
+    void Write(byte[] data);
+    void WriteCutToBuffer();
+    Task FlushToPortAndReset();
+    void SetCharSize(byte widthMul = 1, byte heightMul = 1);
+    void SetWideOnly(bool enable);
+    void ResetLineSpacing();
+    void SetLineSpacing(byte dots);
+    void SetCharacterSpacing(byte dots);
+    void ResetCharacterSpacing();
+    void PrintBitmapEscStar24(string path, int targetWidthPx = 384, byte threshold = 160, bool invert = false);
+    void PrintBitmapEscStar24v2(string path, int targetWidthPx = 384, byte threshold = 160, bool invert = false);
+}
